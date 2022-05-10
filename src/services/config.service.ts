0989fcfb80cfda24148e99d02b4ea5eb03be9d43
j0nl1 <jonlitech@gmail.com>
2022-05-10 20:14:26 +0200

    improve readability

--- src/services/config.service.ts
+++ src/services/config.service.ts
@@ -5,6 +5,7 @@ export const setDefaultKey = (keyName: string): void => {
   const key = getKey(keyName);
   if (!key) throw new Error("Key not found");
   setConfig({ ...config, key });
+  console.log(`Default key set to ${keyName}`);
 };
 
 export const setDefaultNetwork = (networkName: string): void => {
@@ -12,4 +13,5 @@ export const setDefaultNetwork = (networkName: string): void => {
   const network = getNework(networkName);
   if (!network) throw new Error("network not found");
   setConfig({ ...config, network });
+  console.log(`Default network set to ${networkName}`);
 };
