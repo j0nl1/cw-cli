6f29669d126796ce5c40eb60ff2b4169a53c3975
j0nl1 <jonlitech@gmail.com>
2022-05-10 20:11:51 +0200

    fix missing network init dir

--- src/utils/postinstall.ts
+++ src/utils/postinstall.ts
@@ -1,8 +1,9 @@
 import fs from "fs";
-import { KEYS_PATH, CONFIG_PATH, STORAGE_PATH } from "./constants";
+import { STORAGE_PATH, CONFIG_PATH, KEYS_PATH, NETWORKS_PATH } from "./constants";
 
 (() => {
   if (!fs.existsSync(STORAGE_PATH)) fs.mkdirSync(STORAGE_PATH);
   if (!fs.existsSync(KEYS_PATH)) fs.writeFileSync(KEYS_PATH, JSON.stringify({}));
   if (!fs.existsSync(CONFIG_PATH)) fs.writeFileSync(CONFIG_PATH, JSON.stringify({}));
+  if (!fs.existsSync(NETWORKS_PATH)) fs.writeFileSync(NETWORKS_PATH, JSON.stringify({}));
 })();
