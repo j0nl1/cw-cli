f94b21b058fa760affb6694c9f4b8d1cd4719f65
Javier Aceña <40375118+j0nl1@users.noreply.github.com>
2022-05-10 20:01:10 +0200

    feat: storage service (#2)
    * add networks file
    
    * add key interface
    
    * add network interface
    
    * add config interface
    
    * add storage service

---
+++ src/interfaces/config.ts
@@ -0,0 +1,7 @@
+import Network from "./network";
+import Key from "./key";
+
+export default interface Config {
+  network: Network;
+  key: Key;
+}
---
+++ src/interfaces/key.ts
@@ -0,0 +1,7 @@
+export default interface Key {
+  name: string;
+  address: string;
+  pubkey: string;
+  mnemonic: string;
+  type: "local";
+}
