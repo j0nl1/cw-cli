047dd9b4e7b67c77203b2f42b84179fde1b332b8
j0nl1 <jonlitech@gmail.com>
2022-05-11 17:45:44 +0200

    add instantiate method in wasm service

--- src/services/wasm.service.ts
+++ src/services/wasm.service.ts
@@ -6,18 +6,46 @@ import { loadFile } from "../utils/fs";
 import { getKey } from "./keys.service";
 import { getNetwork } from "./networks.service";
 import util from "util";
+import Network from "../interfaces/network";
+import Key from "../interfaces/key";
 
-interface UploadWasmOptions {
-  from: string;
+interface WasmOptions {
+  address: string;
   network: string;
-  memo: string;
+  label: string;
 }
 
-export const uploadWasm = async (filePath: string, { network: chainId, from: addressName, memo }: UploadWasmOptions) => {
+export const uploadWasm = async (filePath: string, opts: WasmOptions) => {
+  const { client, address } = await getSigningClientAndAddress(opts.network, opts.address);
+
+  const wasm = loadFile(filePath);
+
+  if (!wasm) throw new Error("File not found");
+
+  const uploadReceipt = await client.upload(address, wasm, "auto");
+  console.info("Upload succeeded. Receipt:", util.inspect(uploadReceipt, false, null, true));
+};
+
+export const instantiate = async (codeId: string, msg: string, opts: WasmOptions) => {
+  const { client, address } = await getSigningClientAndAddress(opts.network, opts.address);
+
+  const instantiateReceip = await client.instantiate(address, parseInt(codeId), JSON.parse(msg), opts.label, "auto");
+  console.info("Instantiate succeeded. Receipt:", util.inspect(instantiateReceip, false, null, true));
+};
+
+const getNetworkAndKey = async (chainId: string, addressName: string): Promise<{ network: Network; key: Key }> => {
   const network = getNetwork(chainId);
   const key = await getKey(addressName);
 
   if (!network || !key) throw new Error(`Network ${chainId} or key ${addressName} not found`);
+  return { network, key };
+};
+
+const getSigningClientAndAddress = async (
+  chainId: string,
+  addressName: string
+): Promise<{ client: SigningCosmWasmClient; address: string }> => {
+  const { network, key } = await getNetworkAndKey(chainId, addressName);
 
   const wallet = await DirectSecp256k1Wallet.fromKey(fromBase64(key.privkey), network.addressPrefix);
   const [{ address }] = await wallet.getAccounts();
@@ -27,10 +55,5 @@ export const uploadWasm = async (filePath: string, { network: chainId, from: add
     gasPrice: GasPrice.fromString(`${network.gasPrice}${network.feeToken}`)
   });
 
-  const wasm = loadFile(filePath);
-
-  if (!wasm) throw new Error("File not found");
-
-  const uploadReceipt = await client.upload(address, wasm, "auto", memo);
-  console.info("Upload succeeded. Receipt:", util.inspect(uploadReceipt, false, null, true));
+  return { client, address };
 };
